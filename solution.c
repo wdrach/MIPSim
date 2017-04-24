bec82b9a9641aaf5e9aa31bd8e1b9a666a991f2a
Will Drach <will.drach@gmail.com>
2017-04-24 12:07:23 -0600

    more arguments for testing

--- solution.c
+++ solution.c
@@ -2,11 +2,16 @@
 #include "processor.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    printf("USAGE: ./run_sim <program_location>\n");
+  if (argc != 6 && argc != 5) {
+    printf("USAGE: ./run_sim <program_location> <i size> <d size> <block size> [--write_through/-wt]\n");
     return 0;
   }
-  init(argv[1], 64, 1024, 1, false);
+
+  bool wt = false;
+  if (argc == 6 && (strcmp(argv[5], "--write_through") == 0 || strcmp(argv[5], "-wt") == 0)) {
+    wt = true;
+  }
+  init(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), wt);
 
   while(clock())
     ;
